a46e8abe4f191899aa5b5505d22f93997f6f4909
mfs249 <77443409+mfs249@users.noreply.github.com>
2023-05-26 18:56:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function validatePassword(event) {
     alert("Correct password! Access granted.");
 
     // Redirect the user to the WordPress page
-    window.location.href = "https://example.com/wordpress-page"; // Replace with your actual WordPress page URL
+    window.location.href = "https://medium.com/@glngdayu/alocaway-local-transportation-partner-59dac86afc43"; // Replace with your actual WordPress page URL
   } else {
     alert("Incorrect password! Please try again.");
     // Add code here to handle incorrect password scenario
